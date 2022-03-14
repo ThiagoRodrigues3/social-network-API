2af675685d62a778e1b286a5c897517e9356face
Thiago Rod <whytigo@gmail.com>
2022-03-14 19:42:15 -0400

    created the thought route and controller. Tested for effectivness in Insomnia

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require('../../controllers/thought-controller');
+
+router.route('/').get(getThoughts).post(createThought);
+
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(addReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;
