3736ac9342c685492235b8fcfd7ac57d208f4373
Thiago Rod <whytigo@gmail.com>
2022-03-15 19:49:16 -0400

    adjusted some white space

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -9,13 +9,10 @@ const {
   removeFriend,
 } = require('../../controllers/user-controller');
 
-// /api/users
 router.route('/').get(getAllUsers).post(createUser);
 
-// /api/users/:userId
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
-// /api/users/:userId/friends/:friendId
 router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
 module.exports = router;
